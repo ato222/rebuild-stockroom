bf0bf383c89e650451d205afd602d94cdbc6188a
atozzz <yuyuyu0926@icloud.com>
2017-10-17 20:04:29 +0900

    Update to new version and update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rebuild-stockroom
 
 Rebuild.fmの再生位置を保存しておくChrome拡張
+
+（ちなみに、 K or Eneterで、Play/Pause)
--- myscript.js
+++ myscript.js
@@ -1,41 +1,40 @@
-var episode = document.getElementsByTagName('audio')[0];
-var episode_url = episode.src;
-var playButton = document.getElementsByTagName('button')[0];
-var currentTime;
+function rebuildRebuild() {
+  var episode = document.getElementsByTagName('audio')[0];
 
-episode.currentTime = getCurrentTime();
-playButton.focus();
-alertCurrentTimeAndPlay();
+  submitKeyboardShortcut();
+  startEpisode();
+  startSavingLoop();
 
-setInterval(function() {
-    saveCurrentTime();
-}, 1000);
+  function submitKeyboardShortcut() {
+    window.addEventListener("keydown", function(e) {
+      if (e.keyCode === 13 || e.keyCode === 75) {
+        (episode.paused) ? episode.play() : episode.pause();
+      }
+    });
+  }
 
-
-function getCurrentTime() {
-    return (localStorage.getItem(episode_url)) ? localStorage.getItem(episode_url) : 0;
-}
-
-function saveCurrentTime() {
-    localStorage.setItem(episode_url, Math.floor(episode.currentTime));
-}
-
-function getFormattedCurrentTime(second) {
-    var hour = Math.floor(second / 3600);
-    var minute = Math.floor(second / 60);
-    var second = Math.floor(second % 60);
-
-    return paddingTime(hour) + ':' + paddingTime(minute) + ':' + paddingTime(second);
-}
-
-function paddingTime(time) {
-    return ("000"+time).slice(-2);
-}
-
-function alertCurrentTimeAndPlay() {
-    // if (getCurrentTime() != 0) {
-    //     alert('Rebuild Stockroom\nCurrent Time: '+getFormattedCurrentTime(getCurrentTime()));
-    //     episode.play();
-    // }
+  function startEpisode() {
+    episode.currentTime = getCurrentTime();
     episode.play();
+  }
+
+  function startSavingLoop() {
+    setInterval(function() {
+      saveCurrentTime();
+    }, 1000);
+  }
+
+  function getCurrentTime() {
+    var episode_url = episode.src;
+    return (localStorage.getItem(episode_url)) ? localStorage.getItem(episode_url) : 0;
+  }
+
+  function saveCurrentTime() {
+    var episode_url = episode.src;
+    localStorage.setItem(episode_url, Math.floor(episode.currentTime));
+  }
 }
+
+var script = document.createElement("script");
+script.appendChild(document.createTextNode('(' + rebuildRebuild + ")();"));
+document.body.appendChild(script);
